e2fd4294f2e8c11e40137fa425eeb41835d3f0d4
vademich <goncharov1999vadim@gmail.com>
2024-03-05 16:30:38 +0300

    three .less files for main.component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.less'
+  styleUrls: ['./main.component.less', './main.images.less', './main.media.less']
 })
 export class MainComponent {
 
