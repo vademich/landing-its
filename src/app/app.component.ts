512d5207d5f0ecbdc41e9518ca3bf6b14c888bde
vademich <goncharov1999vadim@gmail.com>
2024-03-13 11:46:07 +0300

    меню не перекрывает заголовки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,11 +46,8 @@ export class AppComponent implements OnInit {
 
     if ((this.rect.top-120) * (this.rect.bottom-120) <= 0) {
       document.getElementById(comp)?.style.setProperty("font-weight", `600`);
-      document.getElementById(comp)?.style.setProperty("text-decoration-line", `underline`);
-      document.getElementById(comp)?.style.setProperty("text-decoration-thickness", `2px`);
     } else {
       document.getElementById(comp)?.style.setProperty("font-weight", `400`);
-      document.getElementById(comp)?.style.setProperty("text-decoration", `none`);
     }
   }
 }
